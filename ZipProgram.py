1bc764334cb7894e1cf90f4c7f10abfdd664e126
Kari <92746525+KariSause@users.noreply.github.com>
2022-09-26 12:25:23 +0300

    Update ZipProgram.py

--- ZipProgram.py
+++ ZipProgram.py
@@ -1,7 +1,7 @@
 import zipfile, os
-os.chdir("C:\\Users\\PIV\\Desktop\\Scripts")
-exampleZip = zipfile.ZipFile("CatZip.zip")
+os.chdir("C:\\")
+exampleZip = zipfile.ZipFile("OurZipFile.zip")
 exampleZip.namelist()
-spamInfo = exampleZip.getinfo('Kitty.txt')
+spamInfo = exampleZip.getinfo('Text.txt')
 print('Сжатый файл в %s раза меньше!' % (round(spamInfo.file_size / spamInfo.compress_size, 2)))
 exampleZip.close()
